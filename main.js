67d97ee6d7a7eb36254ff8ca8ed14b710d6588fb
Rakindipe <39384704+rakindipe@users.noreply.github.com>
2026-08-31 13:09:08 -0400

    get photo-author and location from api if no location then don't display anything

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 let imgUrl;
+let imgLocation;
+let imgAuthor;
 
 fetch(
   "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature",
@@ -6,9 +8,25 @@ fetch(
   .then((res) => res.json())
   .then((data) => {
     imgUrl = data.urls.full;
+    imgLocation = data.location.name;
+    imgAuthor = data.user.name;
     document.documentElement.style.setProperty(
       "--bg-image",
       `url("${imgUrl}")`,
     );
-    console.log(imgUrl);
+    console.log(imgLocation);
+    console.log(imgAuthor);
+    // console.log(data);
+
+    if (imgLocation === null) {
+      document.getElementById("author-display").innerHTML = `
+      By: ${imgAuthor}
+    `;
+    } else {
+      document.getElementById("author-display").innerHTML = `
+      ${imgLocation}
+      <br>
+      By: ${imgAuthor}
+    `;
+    }
   });
