17da70e11e6256e5adec6df6c6a06aa8caaa87b2
Rakindipe <39384704+rakindipe@users.noreply.github.com>
2026-09-02 13:59:43 -0400

    wrapped crypto fetch into a function then set it to reload the prices every minute or couple of minutes (will adjust at a later point...)

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let crytpoPrice;
 let crytpoHigh;
 let cryptoLow;
 
-// get bg-image
+/* get bg-image */
 fetch(
   "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature",
 )
@@ -20,8 +20,8 @@ fetch(
       "--bg-image",
       `url("${imgUrl}")`,
     );
-    console.log(imgLocation);
-    console.log(imgAuthor);
+    // console.log(imgLocation);
+    // console.log(imgAuthor);
     // console.log(data);
 
     // don't display "null"
@@ -54,27 +54,33 @@ fetch(
     );
   });
 
-// get crytpo info
-fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data);
+/* get crytpo info */
+const fetchCryptoData = () => {
+  fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
+    .then((res) => res.json())
+    .then((data) => {
+      // console.log(data);
 
-    cryptoName = data.name;
-    crytpoImg = data.image.small;
+      cryptoName = data.name;
+      crytpoImg = data.image.small;
 
-    crytpoPrice = data.market_data.current_price.usd;
-    crytpoHigh = data.market_data.high_24h.usd;
-    cryptoLow = data.market_data.low_24h.usd;
+      crytpoPrice = data.market_data.current_price.usd;
+      crytpoHigh = data.market_data.high_24h.usd;
+      cryptoLow = data.market_data.low_24h.usd;
 
-    document.getElementById("crypto").innerHTML = `
-    <div class="crypto-top">
-      <img src="${crytpoImg}"/>
-      <span>${cryptoName}</span>
-    </div>
-    <p>🎯: $${crytpoPrice.toLocaleString()}</p>
-    <p>📈: $${crytpoHigh.toLocaleString()}</p>
-    <p>📉: $${cryptoLow.toLocaleString()}</p>
-    `;
-  })
-  .catch((err) => console.error(err));
+      document.getElementById("crypto").innerHTML = `
+      <div class="crypto-top">
+        <img src="${crytpoImg}"/>
+        <span>${cryptoName}</span>
+      </div>
+      <p>🎯: $${crytpoPrice.toLocaleString()}</p>
+      <p>📈: $${crytpoHigh.toLocaleString()}</p>
+      <p>📉: $${cryptoLow.toLocaleString()}</p>
+      `;
+    })
+    .catch((err) => console.error(err));
+};
+
+fetchCryptoData();
+
+setInterval(fetchCryptoData, 60000);
