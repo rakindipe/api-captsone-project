649cf0185e19bd00730a1e42861da1b9af9e0569
Rakindipe <39384704+rakindipe@users.noreply.github.com>
2026-09-02 14:27:47 -0400

    moved fetch-bg-image to it's own function

--- main.js
+++ main.js
@@ -8,51 +8,53 @@ let crytpoHigh;
 let cryptoLow;
 
 /* get bg-image */
-fetch(
-  "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature",
-)
-  .then((res) => res.json())
-  .then((data) => {
-    imgUrl = data.urls.full;
-    imgLocation = data.location.name;
-    imgAuthor = data.user.name;
-    document.documentElement.style.setProperty(
-      "--bg-image",
-      `url("${imgUrl}")`,
-    );
-    // console.log(imgLocation);
-    // console.log(imgAuthor);
-    // console.log(data);
+const fetchBgImg = () => {
+  fetch(
+    "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature",
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      imgUrl = data.urls.full;
+      imgLocation = data.location.name;
+      imgAuthor = data.user.name;
+      document.documentElement.style.setProperty(
+        "--bg-image",
+        `url("${imgUrl}")`,
+      );
+      // console.log(imgLocation);
+      // console.log(imgAuthor);
+      // console.log(data);
 
-    // don't display "null"
-    if (imgLocation === null) {
-      document.getElementById("author-display").innerHTML = `
-      <p>
-      ${imgAuthor}
-      </p>
-    `;
-    } else {
-      document.getElementById("author-display").innerHTML = `
-      <p>
-      ${imgLocation}
-      </p>
-      <p>
-      ${imgAuthor}
-      </p>
-    `;
-    }
-  })
-  .catch((err) => {
-    console.error(err);
-    console.log("Something went wrong! 😑");
+      // don't display "null"
+      if (imgLocation === null) {
+        document.getElementById("author-display").innerHTML = `
+        <p>
+        ${imgAuthor}
+        </p>
+      `;
+      } else {
+        document.getElementById("author-display").innerHTML = `
+        <p>
+        ${imgLocation}
+        </p>
+        <p>
+        ${imgAuthor}
+        </p>
+      `;
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      console.log("Something went wrong! 😑");
 
-    imgUrl = "images//default-bg-img.jpeg";
+      imgUrl = "images//default-bg-img.jpeg";
 
-    document.documentElement.style.setProperty(
-      "--bg-image",
-      `url("${imgUrl}")`,
-    );
-  });
+      document.documentElement.style.setProperty(
+        "--bg-image",
+        `url("${imgUrl}")`,
+      );
+    });
+};
 
 /* get crytpo info */
 const fetchCryptoData = () => {
@@ -81,6 +83,7 @@ const fetchCryptoData = () => {
     .catch((err) => console.error(err));
 };
 
+fetchBgImg();
 fetchCryptoData();
 
 setInterval(fetchCryptoData, 60000);
