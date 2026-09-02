2a3294c41ac63bb4fdd83aeaf2d2adb3ed79928a
Rakindipe <39384704+rakindipe@users.noreply.github.com>
2026-09-02 18:00:10 -0400

    added permissions to allow chrome to access user's location

--- main.js
+++ main.js
@@ -144,6 +144,20 @@ const getUserLocation = () => {
       })
       .catch((err) => console.error(err));
   });
+
+  if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log("Latitude:", position.coords.latitude);
+        console.log("Longitude:", position.coords.longitude);
+      },
+      (error) => {
+        console.error("Error getting location:", error.message);
+      },
+    );
+  } else {
+    console.error("Geolocation is not supported by this browser.");
+  }
 };
 
 fetchBgImg();
